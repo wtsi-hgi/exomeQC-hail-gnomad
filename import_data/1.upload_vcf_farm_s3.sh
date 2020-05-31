b60983966db23c7cfb4ad190b75a4975a2678a24
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-05-31 12:21:26 +0100

    added upload vcf farm s3

--- import_data/1.upload_vcf_farm_s3.sh
+++ import_data/1.upload_vcf_farm_s3.sh
@@ -3,8 +3,8 @@
 
  # while read -r FILE; do COUNT=$(( $COUNT + 1 )); gs_name="$(basename "${FILE}" ".gz").${COUNT}.bgz"; s3cmd sync --multipart-chunk-size-mb=100 "${FILE}" "${BUCKET}/${gs_name}"; echo ${COUNT}; done < "${CHUNK}"
 
-declare CHUNK="/lustre/scratch118/humgen/hgi/projects/wtsi_joint_exomes/output_vcf/ddd-elgh-ukbb-vcfs.fofn"
-declare BUCKET="s3://DDD-ELGH-UKBB-exomes/vcfs"
+declare CHUNK="/lustre/scratch118/humgen/hgi/projects/wtsi_joint_exomes/output_vcf/s3_gnomad_upload.fofn"
+declare BUCKET="s3://DDD-ELGH-UKBB-exomes/vcfs-chrs"
 while read -r FILE; do
 	COUNT=$(( $COUNT + 1 ))
 	gs_name="$(basename "${FILE}" ".gz").${COUNT}.bgz"
