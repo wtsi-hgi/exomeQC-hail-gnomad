59097c75342caeafecc333a0699b2112e8df6a18
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-06-04 11:01:55 +0100

    merge matrixtables

---
+++ import_data/3.merge_matrixtables.py
@@ -0,0 +1,87 @@
+import os
+import hail as hl
+import pyspark
+import json
+import sys
+import re
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent
+print(project_root)
+
+s3credentials = os.path.join(
+    project_root, "hail_configuration_files/s3_credentials.json")
+print(s3credentials)
+
+storage = os.path.join(project_root, "hail_configuration_files/storage.json")
+
+thresholds = os.path.join(
+    project_root, "hail_configuration_files/thresholds.json")
+
+with open(f"{s3credentials}", 'r') as f:
+    credentials = json.load(f)
+
+with open(f"{storage}", 'r') as f:
+    storage = json.load(f)
+
+with open(f"{thresholds}", 'r') as f:
+    thresholds = json.load(f)
+
+partitions = 1000
+
+
+
+CHROMOSOMES = ["chr2",
+               "chr3",
+               "chr4",
+               "chr5",
+               "chr6",
+               "chr7",
+               "chr8",
+               "chr9",
+               "chr10",
+               "chr11",
+               "chr12",
+               "chr13",
+               "chr14",
+               "chr15",
+               "chr16",
+               "chr17",
+               "chr18",
+               "chr19",
+               "chr20",
+               "chr21",
+               "chr22",
+               "chrX",
+               "chrY"
+               ]
+
+if __name__ == "__main__":
+    # need to create spark cluster first before intiialising hail
+    sc = pyspark.SparkContext()
+    # Define the hail persistent storage directory
+    tmp_dir = "hdfs://spark-master:9820/"
+    temp_dir = os.path.join(os.environ["HAIL_HOME"], "tmp")
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
+    # you may use your own here from your .s3fg file in your home directory
+    hadoop_config = sc._jsc.hadoopConfiguration()
+
+    hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
+    hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
+
+    #####################################################################
+    ###################### INPUT DATA  ##############################
+    #####################################################################
+    matrixtables_folder = f"{tmp_dir}/ddd-elgh-ukbb"
+
+    print("Reading all matrixtables and joining them")
+    mt = hl.read_matrixtable(f"{tmp_dir}/ddd-elgh-ukbb/chr1.mt")
+    for chromosome in CHROMOSOMES:
+        mt2 = hl.read_matrixtable(f"{tmp_dir}/ddd-elgh-ukbb/{chromosome}.mt")
+        mt = mt.union_rows(mt2)
+
+    print("Now writing joined matrixtable to disk:")
+
+    mt.write(f"{tmp_dir}/ddd-elgh-ukbb/WGS-ddd-elgh-ukbb.mt", overwrite=True)
+    print(f"Wrote matrixtable for whole genome.")
