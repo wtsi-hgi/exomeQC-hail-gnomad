d39ab06d9eb82f40a566aa440900a421988c2b1a
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-06-12 12:58:28 +0100

    added new cohort info files

--- import_data/create_map_file.py
+++ import_data/create_map_file.py
@@ -45,7 +45,7 @@ with open(map_file4, newline='') as samples4:
     samples_reader = csv.reader(samples4, delimiter=' ')
     for s1 in samples_reader:
         if s1[0] in samples_list.keys() and "ELGH" not in samples_list[s1[0]]:
-            if "IHTP_ISC_British" in s1[2]:
+            if "IHTP_ISC_British" in s1[2] or "SEQCAP_WES_AUTOZYGOSITY_Born_in_Bradford" in s1[2]:
                 samples_list[s1[0]] = ELGH_1
             else:
                 samples_list[s1[0]] = s1[2]
