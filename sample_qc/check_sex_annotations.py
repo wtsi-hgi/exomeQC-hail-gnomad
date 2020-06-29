8707734f08fd145fb732dbc6128b76bc05862a64
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-06-29 16:17:30 +0100

    validation of sex annotations

--- sample_qc/check_sex_annotations.py
+++ sample_qc/check_sex_annotations.py
@@ -17,6 +17,10 @@ DDD = "/nfs/users/nfs_m/mercury/pa10/ddd-elgh-ukbb/sex_annotations/DDD_ega_gende
 # EGAN00001351470	F
 
 elgh = "/nfs/users/nfs_m/mercury/pa10/ddd-elgh-ukbb/sex_annotations/ELGH_samples_with_coverage_and_assigned_sex.txt"
+# sample	bam	total	chrX	chrY	batch	SupplierID	EGAID	Sex	blah	low.coverage	Y.to.X	sex.assigned
+# ABF5580478	2159967.cram	40.9141	43.196	4.37683	1	D00_20354	EGAN00001175140	Unknown	NULL	FALSE	0.101324891193629	female
+# ABF5580479	2159968.cram	47.3553	26.7831	29.3019	1	D01_16627	EGAN00001175141	Unknown	NULL	FALSE	1.09404437873136	male
+
 ukbb = "/nfs/users/nfs_m/mercury/pa10/ddd-elgh-ukbb/sex_annotations/UKBB_sex_at_recruitment_EGAN.txt"
 
 df = pd.read_csv(hail_results, compression='gzip', delimiter="\t")
@@ -33,3 +37,8 @@ print(df_elgh.head())
 
 df_ukbb = pd.read_csv(ukbb, delimiter="\t")
 print(df_ukbb.head())
+
+# merge1:
+df1 = pd.merge(df, df_ddd, how='left', left_on="s", right_on="ega_id")
+
+print(df1.head())
