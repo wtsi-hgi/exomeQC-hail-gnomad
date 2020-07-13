791743368837a80350750927046d58c03c04a17a
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-07-13 14:06:22 +0100

    added gnomad ancestry code

---
+++ sample_qc_v3/ancestry.py
@@ -0,0 +1,108 @@
+import os
+import hail as hl
+import pandas as pd 
+import pyspark
+import json
+import sys
+import re
+from pathlib import Path
+import logging
+from typing import List, Tuple
+from bokeh.plotting import output_file, save, show
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+POP_NAMES = {
+    "afr": "African/African-American",
+    "ami": "Amish",
+    "amr": "Latino",
+    "asj": "Ashkenazi Jewish",
+    "eas": "East Asian",
+    "eur": "European",
+    "fin": "Finnish",
+    "mde": "Middle Eastern",
+    "nfe": "Non-Finnish European",
+    "oth": "Other",
+    "sas": "South Asian",
+    "uniform": "Uniform",
+    "sas_non_consang": "South Asian (F < 0.05)",
+    "consanguineous": "South Asian (F > 0.05)",
+    "exac": "ExAC",
+    "bgr": "Bulgarian (Eastern European)",
+    "est": "Estonian",
+    "gbr": "British",
+    "nwe": "North-Western European",
+    "seu": "Southern European",
+    "swe": "Swedish",
+    "kor": "Korean",
+    "sgp": "Singaporean",
+    "jpn": "Japanese",
+    "oea": "Other East Asian",
+    "oeu": "Other European",
+    "onf": "Other Non-Finnish European",
+    "unk": "Unknown",
+}
+
+POP_COLORS = {
+    "afr": "#941494",
+    "ami": "#FFC0CB",
+    "amr": "#ED1E24",
+    "asj": "#FF7F50",
+    "eas": "#108C44",
+    "eur": "#6AA5CD",
+    "fin": "#002F6C",
+    "mde": "#33CC33",
+    "nfe": "#6AA5CD",
+    "oth": "#ABB9B9",
+    "sas": "#FF9912",
+    "uniform": "pink",
+    "consanguineous": "pink",
+    "sas_non_consang": "orange",
+    "exac": "gray",
+    "bgr": "#66C2A5",
+    "est": "black",
+    "gbr": "#C60C30",
+    "nwe": "#C60C30",
+    "seu": "#3CA021",
+    "swe": "purple",
+    "kor": "#4891D9",
+    "sgp": "darkred",
+    "jpn": "#BC002D",
+    "oea": "#108C44",
+    "oeu": "#6AA5CD",
+    "onf": "#6AA5CD",
+    "unk": "#ABB9B9",
+    "": "#ABB9B9",
+}
+
+
+
+
+
+
+if __name__ == "__main__":
+    # need to create spark cluster first before intiialising hail
+    sc = pyspark.SparkContext()
+    # Define the hail persistent storage directory
+    tmp_dir = "hdfs://spark-master:9820/"
+    temp_dir = os.path.join(os.environ["HAIL_HOME"], "tmp")
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
+    # you may use your own here from your .s3fg file in your home directory
+    hadoop_config = sc._jsc.hadoopConfiguration()
+
+    hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
+    hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
+
+mt = hl.read_matrix_table(
+    #    f"{temp_dir}/ddd-elgh-ukbb/chr1_chr20_XY_sex_annotations.mt")
+
+    # ld pruning
+    #pruned_ht = hl.ld_prune(mt.GT, r2=0.1)
+    #pruned_mt = mt.filter_rows(hl.is_defined(pruned_ht[mt.row_key]))
+    # pruned_mt.write(
+    #    f"{tmp_dir}/ddd-elgh-ukbb/chr1_chr20_XY_ldpruned.mt", overwrite=True)
+    pruned_mt = hl.read_matrix_table(
+        f"{temp_dir}/ddd-elgh-ukbb/chr1_chr20_XY_ldpruned.mt")
+
