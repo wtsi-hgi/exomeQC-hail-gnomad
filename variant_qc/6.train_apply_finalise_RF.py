691a0d833d45f544772783e6b8a6cdd9721a0990
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-10-12 17:28:08 +0100

    apply rf to all the variants in dataset

--- variant_qc/6.train_apply_finalise_RF.py
+++ variant_qc/6.train_apply_finalise_RF.py
@@ -531,7 +531,7 @@ def main(args):
         #ht = get_rf(data="training", run_hash=run_hash).ht()
         features = hl.eval(rf_model.features)
         #ht = apply_rf_model(ht, rf_model, features, label=LABEL_COL)
-        ht = apply_rf_model(ht, rf_model, features)
+        ht = apply_rf_model(ht, rf_model, features, label=None)
         logger.info("Finished applying RF model")
         ht = ht.annotate_globals(rf_hash=run_hash)
         ht = ht.checkpoint(
