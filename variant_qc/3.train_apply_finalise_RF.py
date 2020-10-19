61fbbceb140596fbed8f9ce6b147a7d8c0ddfc4c
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-10-19 16:37:23 +0100

    add 3.train_apply_finalise_RF.py to run all steps

--- variant_qc/3.train_apply_finalise_RF.py
+++ variant_qc/3.train_apply_finalise_RF.py
@@ -477,7 +477,7 @@ def train_rf_model(
     ht = ht.select_globals(
         features_importance=features_importance,
         features=rf_features,
-        test_results=test_results,
+        #  test_results=test_results,
     )
 
     return ht.select("rf_train", "rf_label", "rf_test"), rf_model
