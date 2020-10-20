c8c7e9364c110b5a764c1c158ce66e19316590ae
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-10-20 10:43:27 +0100

    add 3.train_apply_finalise_RF.py to run all steps

--- variant_qc/3.train_apply_finalise_RF.py
+++ variant_qc/3.train_apply_finalise_RF.py
@@ -229,7 +229,7 @@ def train_rf(ht, args):
         rf_features=features,
         tp_expr=ht.tp,
         fp_expr=ht.fp,
-        fp_to_tp=args.fp_to_tp,
+        fp_to_tp=1,
         num_trees=args.num_trees,
         max_depth=args.max_depth,
         test_expr=hl.literal(test_intervals).any(
