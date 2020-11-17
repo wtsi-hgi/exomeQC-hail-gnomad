a1b4a62d4726f13094320386b7237c5209855b43
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-11-17 10:46:07 +0000

    family stats

--- variant_qc/1a.generate_de_novo.py
+++ variant_qc/1a.generate_de_novo.py
@@ -514,27 +514,28 @@ if __name__ == "__main__":
 
     mt = hl.read_matrix_table(
         f'{temp_dir}/ddd-elgh-ukbb/variant_qc/Sanger_cohorts_chr1-7and20_split.mt')
-    fam = "s3a://DDD-ELGH-UKBB-exomes/trios/DDD_trios.fam"
+    fam = f"{temp_dir}/variant_qc/DDD_trios.fam"
     pedigree = hl.Pedigree.read(fam)
     (mt1, famstats_ht) = generate_family_stats(mt, fam)
     print("Writing mt and family stats_ht")
     mt1.write(f'{tmp_dir}/Sanger_cohorts_family_stats.mt', overwrite=True)
-    famstats_ht.write(f'{tmp_dir}/Sanger_cohorts_family_stats.ht',overwrite=True)
+    famstats_ht.write(
+        f'{tmp_dir}/Sanger_cohorts_family_stats.ht', overwrite=True)
     #mt = hl.variant_qc(mt)
     # mt=annotate_freq(mt)
     # trio annotation:
     #mt_adj = annotate_adj(mt)
-    fam = "s3a://DDD-ELGH-UKBB-exomes/trios/DDD_trios.fam"
-    pedigree = hl.Pedigree.read(fam)
+    #pedigree = hl.Pedigree.read(fam)
+    ##fam = f"{temp_dir}/variant_qc/DDD_trios.fam"
     #trio_dataset = hl.trio_matrix(mt_adj, pedigree, complete_trios=True)
-    trio_dataset = hl.read_matrix_table(
-        f'{temp_dir}/ddd-elgh-ukbb/variant_qc/mt_trios_adj.mt')
+    # trio_dataset = hl.read_matrix_table(
+    #    f'{temp_dir}/ddd-elgh-ukbb/variant_qc/mt_trios_adj.mt')
 
     # trio_dataset=annotate_freq(trio_dataset)
     #trio_dataset = trio_dataset.annotate_rows(family_stats=freq_data[trio_dataset.row_key].family_stats)
-    de_novo_table = hl.de_novo(
-        mt, pedigree, trio_dataset.family_stats[0].unrelated_qc_callstats.AF[1])
-    de_novo_table = de_novo_table.key_by(
-        'locus', 'alleles').collect_by_key('de_novo_data')
-    de_novo_table.write(
-        f'{tmp_dir}/Sanger_cohorts_denovo_table.ht', overwrite=True)
+    # de_novo_table = hl.de_novo(
+    #    mt, pedigree, trio_dataset.family_stats[0].unrelated_qc_callstats.AF[1])
+    # de_novo_table = de_novo_table.key_by(
+    #    'locus', 'alleles').collect_by_key('de_novo_data')
+    # de_novo_table.write(
+    #    f'{tmp_dir}/Sanger_cohorts_denovo_table.ht', overwrite=True)
