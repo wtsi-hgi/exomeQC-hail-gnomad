b19da570663e6bf81c6d5d7fb8db8924e77d67f2
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-11-17 10:27:51 +0000

    de novo script

--- variant_qc/1a.generate_de_novo.py
+++ variant_qc/1a.generate_de_novo.py
@@ -77,6 +77,227 @@ TRUTH_DATA = ["hapmap", "omni", "mills", "kgp_phase1_hc"]
 INBREEDING_COEFF_HARD_CUTOFF = -0.3
 
 
+def annotate_freq(
+    mt: hl.MatrixTable,
+    sex_expr: Optional[hl.expr.StringExpression] = None,
+    pop_expr: Optional[hl.expr.StringExpression] = None,
+    subpop_expr: Optional[hl.expr.StringExpression] = None,
+    additional_strata_expr: Optional[Dict[str, hl.expr.StringExpression]] = None,
+    downsamplings: Optional[List[int]] = None,
+) -> hl.MatrixTable:
+    """
+    Adds a row annotation `freq` to the input `mt` with stratified allele frequencies,
+    and a global annotation `freq_meta` with metadata.
+
+    .. note::
+
+        Currently this only supports bi-allelic sites.
+        The input `mt` needs to have the following entry fields:
+        - GT: a CallExpression containing the genotype
+        - adj: a BooleanExpression containing whether the genotype is of high quality or not.
+        All expressions arguments need to be expression on the input `mt`.
+
+    .. rubric:: `freq` row annotation
+
+    The `freq` row annotation is an Array of Struct, with each Struct containing the following fields:
+
+        - AC: int32
+        - AF: float64
+        - AN: int32
+        - homozygote_count: int32
+
+    Each element of the array corresponds to a stratification of the data, and the metadata about these annotations is
+    stored in the globals.
+
+    .. rubric:: Global `freq_meta` metadata annotation
+
+    The global annotation `freq_meta` is added to the input `mt`. It is a list of dict.
+    Each element of the list contains metadata on a frequency stratification and the index in the list corresponds
+    to the index of that frequency stratification in the `freq` row annotation.
+
+    .. rubric:: The `downsamplings` parameter
+
+    If the `downsamplings` parameter is used, frequencies will be computed for all samples and by population
+    (if `pop_expr` is specified) by downsampling the number of samples without replacement to each of the numbers specified in the
+    `downsamplings` array, provided that there are enough samples in the dataset.
+    In addition, if `pop_expr` is specified, a downsampling to each of the exact number of samples present in each population is added.
+    Note that samples are randomly sampled only once, meaning that the lower downsamplings are subsets of the higher ones.
+
+    :param mt: Input MatrixTable
+    :param sex_expr: When specified, frequencies are stratified by sex. If `pop_expr` is also specified, then a pop/sex stratifiction is added.
+    :param pop_expr: When specified, frequencies are stratified by population. If `sex_expr` is also specified, then a pop/sex stratifiction is added.
+    :param subpop_expr: When specified, frequencies are stratified by sub-continental population. Note that `pop_expr` is required as well when using this option.
+    :param additional_strata_expr: When specified, frequencies are stratified by the given additional strata found in the dict. This can e.g. be used to stratify by platform.
+    :param downsamplings: When specified, frequencies are computed by downsampling the data to the number of samples given in the list. Note that if `pop_expr` is specified, downsamplings by population is also computed.
+    :return: MatrixTable with `freq` annotation
+    """
+
+    if subpop_expr is not None and pop_expr is None:
+        raise NotImplementedError(
+            "annotate_freq requires pop_expr when using subpop_expr"
+        )
+
+    if additional_strata_expr is None:
+        additional_strata_expr = {}
+
+    _freq_meta_expr = hl.struct(**additional_strata_expr)
+    if sex_expr is not None:
+        _freq_meta_expr = _freq_meta_expr.annotate(sex=sex_expr)
+    if pop_expr is not None:
+        _freq_meta_expr = _freq_meta_expr.annotate(pop=pop_expr)
+    if subpop_expr is not None:
+        _freq_meta_expr = _freq_meta_expr.annotate(subpop=subpop_expr)
+
+    # Annotate cols with provided cuts
+    mt = mt.annotate_cols(_freq_meta=_freq_meta_expr)
+
+    # Get counters for sex, pop and subpop if set
+    cut_dict = {
+        cut: hl.agg.filter(
+            hl.is_defined(mt._freq_meta[cut]), hl.agg.counter(
+                mt._freq_meta[cut])
+        )
+        for cut in mt._freq_meta
+        if cut != "subpop"
+    }
+    if "subpop" in mt._freq_meta:
+        cut_dict["subpop"] = hl.agg.filter(
+            hl.is_defined(mt._freq_meta.pop) & hl.is_defined(
+                mt._freq_meta.subpop),
+            hl.agg.counter(
+                hl.struct(subpop=mt._freq_meta.subpop, pop=mt._freq_meta.pop)
+            ),
+        )
+
+    cut_data = mt.aggregate_cols(hl.struct(**cut_dict))
+    sample_group_filters = []
+
+    # Create downsamplings if needed
+    if downsamplings is not None:
+        # Add exact pop size downsampling if pops were provided
+        if cut_data.get("pop"):
+            downsamplings = list(
+                set(downsamplings + list(cut_data.get("pop").values()))
+            )  # Add the pops values if not in yet
+            downsamplings = sorted(
+                [x for x in downsamplings if x <= sum(
+                    cut_data.get("pop").values())]
+            )
+        logger.info(
+            f"Found {len(downsamplings)} downsamplings: {downsamplings}")
+
+        # Shuffle the samples, then create a global index for downsampling
+        # And a pop-index if pops were provided
+        downsampling_ht = mt.cols()
+        downsampling_ht = downsampling_ht.annotate(r=hl.rand_unif(0, 1))
+        downsampling_ht = downsampling_ht.order_by(downsampling_ht.r)
+        scan_expr = {"global_idx": hl.scan.count()}
+        if cut_data.get("pop"):
+            scan_expr["pop_idx"] = hl.scan.counter(downsampling_ht._freq_meta.pop).get(
+                downsampling_ht._freq_meta.pop, 0
+            )
+        downsampling_ht = downsampling_ht.annotate(**scan_expr)
+        downsampling_ht = downsampling_ht.key_by("s").select(*scan_expr)
+        mt = mt.annotate_cols(downsampling=downsampling_ht[mt.s])
+        mt = mt.annotate_globals(downsamplings=downsamplings)
+
+        # Create downsampled sample groups
+        sample_group_filters.extend(
+            [
+                (
+                    {"downsampling": str(ds), "pop": "global"},
+                    mt.downsampling.global_idx < ds,
+                )
+                for ds in downsamplings
+            ]
+        )
+        if cut_data.get("pop"):
+            sample_group_filters.extend(
+                [
+                    (
+                        {"downsampling": str(ds), "pop": pop},
+                        (mt.downsampling.pop_idx < ds) & (
+                            mt._freq_meta.pop == pop),
+                    )
+                    for ds in downsamplings
+                    for pop, pop_count in cut_data.get("pop", {}).items()
+                    if ds <= pop_count
+                ]
+            )
+
+    # Add all desired strata, starting with the full set and ending with downsamplings (if any)
+    sample_group_filters = (
+        [({}, True)]
+        + [({"pop": pop}, mt._freq_meta.pop == pop)
+            for pop in cut_data.get("pop", {})]
+        + [({"sex": sex}, mt._freq_meta.sex == sex)
+            for sex in cut_data.get("sex", {})]
+        + [
+            (
+                {"pop": pop, "sex": sex},
+                (mt._freq_meta.sex == sex) & (mt._freq_meta.pop == pop),
+            )
+            for sex in cut_data.get("sex", {})
+            for pop in cut_data.get("pop", {})
+        ]
+        + [
+            (
+                {"subpop": subpop.subpop, "pop": subpop.pop},
+                (mt._freq_meta.pop == subpop.pop)
+                & (mt._freq_meta.subpop == subpop.subpop),
+            )
+            for subpop in cut_data.get("subpop", {})
+        ]
+        + [
+            ({strata: str(s_value)}, mt._freq_meta[strata] == s_value)
+            for strata in additional_strata_expr
+            for s_value in cut_data.get(strata, {})
+        ]
+        + sample_group_filters
+    )
+
+    # Annotate columns with group_membership
+    mt = mt.annotate_cols(group_membership=[x[1]
+                                            for x in sample_group_filters])
+
+    # Create and annotate global expression with meta information
+    freq_meta_expr = [
+        dict(**sample_group[0], group="adj") for sample_group in sample_group_filters
+    ]
+    freq_meta_expr.insert(1, {"group": "raw"})
+    mt = mt.annotate_globals(freq_meta=freq_meta_expr)
+
+    # Create frequency expression array from the sample groups
+    freq_expr = hl.agg.array_agg(
+        lambda i: hl.agg.filter(
+            mt.group_membership[i] & mt.adj, hl.agg.call_stats(
+                mt.GT, mt.alleles)
+        ),
+        hl.range(len(sample_group_filters)),
+    )
+
+    # Insert raw as the second element of the array
+    freq_expr = (
+        freq_expr[:1]
+        .extend([hl.agg.call_stats(mt.GT, mt.alleles)])
+        .extend(freq_expr[1:])
+    )
+
+    # Select non-ref allele (assumes bi-allelic)
+    freq_expr = freq_expr.map(
+        lambda cs: cs.annotate(
+            AC=cs.AC[1],
+            AF=cs.AF[
+                1
+            ],  # TODO This is NA in case AC and AN are 0 -- should we set it to 0?
+            homozygote_count=cs.homozygote_count[1],
+        )
+    )
+
+    # Return MT with freq row annotation
+    return mt.annotate_rows(freq=freq_expr).drop("_freq_meta")
+
+
 def get_truth_ht() -> Table:
     """
     Returns a table with the following annotations from the latest version of the corresponding truth data:
@@ -329,17 +550,21 @@ if __name__ == "__main__":
 
     mt = hl.read_matrix_table(
         f'{temp_dir}/ddd-elgh-ukbb/variant_qc/Sanger_cohorts_chr1-7and20_split.mt')
-
-    mt = hl.variant_qc(mt)
-
-    # trio annotation:
-    mt_adj = annotate_adj(mt)
     fam = "s3a://DDD-ELGH-UKBB-exomes/trios/DDD_trios.fam"
     pedigree = hl.Pedigree.read(fam)
-    trio_dataset = hl.trio_matrix(mt_adj, pedigree, complete_trios=True)
+    (mt1, stats_ht) = generate_family_stats(mt, fam)
+    #mt = hl.variant_qc(mt)
+    # mt=annotate_freq(mt)
+    # trio annotation:
+    #mt_adj = annotate_adj(mt)
+    fam = "s3a://DDD-ELGH-UKBB-exomes/trios/DDD_trios.fam"
+    pedigree = hl.Pedigree.read(fam)
+    #trio_dataset = hl.trio_matrix(mt_adj, pedigree, complete_trios=True)
     trio_dataset = hl.read_matrix_table(
         f'{temp_dir}/ddd-elgh-ukbb/variant_qc/mt_trios_adj.mt')
 
+    # trio_dataset=annotate_freq(trio_dataset)
+    #trio_dataset = trio_dataset.annotate_rows(family_stats=freq_data[trio_dataset.row_key].family_stats)
     de_novo_table = hl.de_novo(
         mt, pedigree, trio_dataset.family_stats[0].unrelated_qc_callstats.AF[1])
     de_novo_table = de_novo_table.key_by(
