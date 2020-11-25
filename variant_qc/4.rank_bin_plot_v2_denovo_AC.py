02b2ca19f01744175dfe396b767ab6cbece5b56d
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-11-25 14:51:29 +0000

    AC for ranking

--- variant_qc/4.rank_bin_plot_v2_denovo_AC.py
+++ variant_qc/4.rank_bin_plot_v2_denovo_AC.py
@@ -369,12 +369,12 @@ def create_binned_data_initial(ht: hl.Table, data: str, data_type: str, n_bins: 
             #    ht.family_stats.unrelated_qc_callstats.AC[1] == 0), hl.agg.count_where(ht.family_stats.mendel.errors > 0)),
             n_trans_singletons=hl.agg.filter((ht.ac_raw < 3) & (
                 ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1), hl.agg.sum(ht.family_stats.tdt[0].t)),
-            n_trans_singletons_synonymous=hl.agg.filter((ht.AC[0] < 3) & (ht.consequence == "synonymous_variant") & (
-                ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1), hl.agg.sum(ht.family_stats.tdt[0].t)),
+            n_trans_singletons_synonymous=hl.agg.filter((ht.ac_raw < 3) & (ht.consequence == "synonymous_variant") & (
+                ht.AC[0] == 2), hl.agg.sum(ht.family_stats.tdt[0].t)),
             n_untrans_singletons=hl.agg.filter((ht.ac_raw < 3) & (
                 ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1), hl.agg.sum(ht.family_stats.tdt[0].u)),
-            n_untrans_singletons_synonymous=hl.agg.filter((ht.AC[0] < 3) & (ht.consequence == "synonymous_variant") & (
-                ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1), hl.agg.sum(ht.family_stats.tdt[0].u)),
+            n_untrans_singletons_synonymous=hl.agg.filter((ht.ac_raw < 3) & (ht.consequence == "synonymous_variant") & (
+                ht.AC[0] == 1), hl.agg.sum(ht.family_stats.tdt[0].u)),
             n_train_trans_singletons=hl.agg.count_where(
                 (ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1) & (ht.family_stats.tdt[0].t == 1)),
             n_omni=hl.agg.count_where(ht.omni),
