3d6eb1856ef8e300769bce8e5bee5a8ff5388343
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-12-02 13:50:26 +0000

    filter mt

---
+++ kousik_filter.py
@@ -0,0 +1,66 @@
+# Pavlos Antoniou
+# 16/09/2020
+#  trio matrixtable creation from fam file
+import os
+import hail as hl
+import pandas as pd
+import pyspark
+import json
+import sys
+import re
+from pathlib import Path
+import logging
+from typing import Any, Counter, List, Optional, Tuple, Union
+from bokeh.plotting import output_file, save, show
+
+from hail import Table
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+project_root = Path(__file__).parent.parent
+print(project_root)
+
+s3credentials = os.path.join(
+    project_root, "hail_configuration_files/s3_credentials.json")
+print(s3credentials)
+
+storage = os.path.join(project_root, "hail_configuration_files/storage.json")
+
+thresholds = os.path.join(
+    project_root, "hail_configuration_files/thresholds.json")
+
+with open(f"{s3credentials}", 'r') as f:
+    credentials = json.load(f)
+
+with open(f"{storage}", 'r') as f:
+    storage = json.load(f)
+
+with open(f"{thresholds}", 'r') as f:
+    thresholds = json.load(f)
+
+
+if __name__ == "__main__":
+    # need to create spark cluster first before intiialising hail
+    sc = pyspark.SparkContext()
+    # Define the hail persistent storage directory
+    tmp_dir = "hdfs://spark-master:9820/"
+    temp_dir = "file:///home/ubuntu/data/tmp"
+    plot_dir = "/home/ubuntu/data/tmp"
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
+    # you may use your own here from your .s3fg file in your home directory
+    hadoop_config = sc._jsc.hadoopConfiguration()
+
+    hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
+    hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
+
+    ################################
+
+    ht = hl.read_table(f"{temp_dir}/intervalwgs/variant_list.ht")
+    mt = hl.read_matrix_table(
+        f'{temp_dir}/intervalwgs/WGS_final_march_2020_dbsnp_v53.mt')
+    mt1 = mt.filter_rows(hl.is_defined(ht[mt.locus]))
+    mt1.write(f"{tmp_dir}/intervalwgs/variant_list_for_kousik.mt",
+              overwrite=True)
