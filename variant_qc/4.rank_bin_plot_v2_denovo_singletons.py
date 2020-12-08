257a4cdd327cdc15c350a0119efbe3a270fcd820
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-12-08 16:47:51 +0000

    rank bin singletons

--- variant_qc/4.rank_bin_plot_v2_denovo_singletons.py
+++ variant_qc/4.rank_bin_plot_v2_denovo_singletons.py
@@ -370,10 +370,10 @@ def create_binned_data_initial(ht: hl.Table, data: str, data_type: str, n_bins: 
             n_trans_singletons=hl.agg.filter((ht.ac_raw < 3) & (
                 ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1), hl.agg.sum(ht.family_stats.tdt[0].t)),
             n_trans_singletons_synonymous=hl.agg.filter(
-                ht.transmitted_singletons == 1),
+                ht.transmitted_singletons == 1, hl.agg.sum(ht.transmitted_singletons == 1)),
             n_untrans_singletons=hl.agg.filter((ht.ac_raw < 3) & (
                 ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1), hl.agg.sum(ht.family_stats.tdt[0].u)),
-            n_untrans_singletons_synonymous=hl.agg.filter(
+            n_untrans_singletons_synonymous=hl.agg.count_where(
                 ht.untransmitted_singletons == 1),
             n_train_trans_singletons=hl.agg.count_where(
                 (ht.family_stats.unrelated_qc_callstats.AC[0][1] == 1) & (ht.family_stats.tdt[0].t == 1)),
