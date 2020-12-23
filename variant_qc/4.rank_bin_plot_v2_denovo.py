4963e22745fc3566693e26e474f9abd4810bd5b5
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-12-23 13:43:12 +0000

    ddd denovos

--- variant_qc/4.rank_bin_plot_v2_denovo.py
+++ variant_qc/4.rank_bin_plot_v2_denovo.py
@@ -353,6 +353,8 @@ def create_binned_data_initial(ht: hl.Table, data: str, data_type: str, n_bins: 
             n_singleton=hl.agg.count_where(ht.transmitted_singleton),
             n_high_quality_de_novos=hl.agg.count_where(
                 ht.de_novo_data.p_de_novo[0] > 0.99),
+            n_validated_DDD_denovos=hl.agg.count_where(
+                ht.inheritance.contains("De novo")),
             n_medium_quality_de_novos=hl.agg.count_where(
                 ht.de_novo_data.p_de_novo[0] > 0.5),
             n_high_confidence_de_novos=hl.agg.count_where(
@@ -399,7 +401,7 @@ def main(args):
     # ht after random model
     run_hash = args.run_hash
     ht = hl.read_table(
-        f'{temp_dir}/ddd-elgh-ukbb/variant_qc/models/{run_hash}/{run_hash}_rf_result_sanger_cohorts_DENOVO_family_stats_SYNONYMOUS.ht')
+        f'{temp_dir}/ddd-elgh-ukbb/variant_qc/models/{run_hash}/{run_hash}_rf_result_sanger_cohorts_DENOVO_family_stats_SYNONYMOUS_inheritance.ht')
 
     if args.add_rank:
         ht_ranked = add_rank(ht,
@@ -421,14 +423,14 @@ def main(args):
         # ht_ranked = ht_ranked.annotate(score=1-ht_ranked.rf_probability["TP"])
         ht_ranked = ht_ranked.annotate(score=ht_ranked.rf_probability["TP"])
         ht_ranked = ht_ranked.checkpoint(
-            f'{tmp_dir}/ddd-elgh-ukbb/{run_hash}_rf_result_ranked_denovo.ht', overwrite=True)
+            f'{tmp_dir}/ddd-elgh-ukbb/{run_hash}_rf_result_ranked_denovo_ddd.ht', overwrite=True)
 
     if args.add_bin:
 
         # ht = hl.read_table(
         #    f'{temp_dir}/ddd-elgh-ukbb/variant_qc/models/{run_hash}/{run_hash}_rf_result_ranked.ht')
         ht = hl.read_table(
-            f'{temp_dir}/ddd-elgh-ukbb/variant_qc/models/{run_hash}/{run_hash}_rf_result_ranked_denovo.ht')
+            f'{temp_dir}/ddd-elgh-ukbb/variant_qc/models/{run_hash}/{run_hash}_rf_result_ranked_denovo_ddd.ht')
 
         # ht_bins = compute_quantile_bin(ht, ht.rf_probability["TP"], bin_expr={
         #    'biallelic_bin': ~ht.was_split,
@@ -436,7 +438,7 @@ def main(args):
         # }, compute_snv_indel_separately=True, n_bins=100, k=500, desc=True)
         ht_bins = create_binned_data_initial(ht, "exomes", "RF", n_bins=100)
         ht_bins.write(
-            f'{tmp_dir}/ddd-elgh-ukbb/{run_hash}_rf_result_ranked_BINS_denovo.ht', overwrite=True)
+            f'{tmp_dir}/ddd-elgh-ukbb/{run_hash}_rf_result_ranked_BINS_denovo_ddd.ht', overwrite=True)
         # ht_grouped = compute_grouped_binned_ht(ht_bins)
         # ht_grouped.write(
         #    f'{tmp_dir}/ddd-elgh-ukbb/{run_hash}_rf_result_ranked_BINS_Grouped.ht', overwrite=True)
