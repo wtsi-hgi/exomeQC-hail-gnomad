370a57cba33336ce7efe99ffc573e7c49e0366e5
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-03-15 11:12:11 +0000

    sample qc  script 5.  done

--- sample_qc_v3/5.filter_samples_fail_sample_qc.py
+++ sample_qc_v3/5.filter_samples_fail_sample_qc.py
@@ -11,8 +11,13 @@ import re
 from pathlib import Path
 import logging
 from typing import Any, Counter, List, Optional, Tuple, Union
-from bokeh.plotting import output_file, save, show
 from gnomad.resources.grch38 import gnomad
+import argparse
+
+tmp_dir = "hdfs://spark-master:9820/"
+temp_dir = "file:///home/ubuntu/data/tmp"
+plot_dir = "/home/ubuntu/data/tmp"
+
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -39,13 +44,22 @@ with open(f"{thresholds}", 'r') as f:
     thresholds = json.load(f)
 
 
+def main(args):
+    mt = hl.read_matrix_table(
+        args.matrixtable)
+    samples_to_remove_filename = args.failed_samples_list
+
+    samples_to_remove = hl.import_table(samples_to_remove_filename).key_by('s')
+    mt_filtered = mt.filter_cols(hl.is_defined(
+        samples_to_remove[mt.s]), keep=False)
+    mt_filtered.write(
+        f'{args.output_dir}/ddd-elgh-ukbb/Sanger_cohorts_chr1-7and20_split_sampleqc_filtered.mt')
+
+
 if __name__ == "__main__":
     # need to create spark cluster first before intiialising hail
     sc = pyspark.SparkContext()
     # Define the hail persistent storage directory
-    tmp_dir = "hdfs://spark-master:9820/"
-    temp_dir = "file:///home/ubuntu/data/tmp"
-    plot_dir = "/home/ubuntu/data/tmp"
     hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
     # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
     # you may use your own here from your .s3fg file in your home directory
@@ -54,15 +68,43 @@ if __name__ == "__main__":
     hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
     hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
 
-    # read matrixtable = remove the
-    # mt = hl.read_matrix_table(
-    #    f'{temp_dir}/ddd-elgh-ukbb/Sanger_cohorts_chr1-20-XY_new_cohorts_split_multi.mt')
-    mt = hl.read_matrix_table(
-        f'{temp_dir}/ddd-elgh-ukbb/variant_qc/Sanger_cohorts_chr1-7and20_split.mt')
-    samples_to_remove_filename = f"{temp_dir}/ddd-elgh-ukbb/filtering/samples_failed_QC.tsv"
+    # need to create spark cluster first before intiialising hail
+    sc = pyspark.SparkContext()
+    # Define the hail persistent storage directory
 
-    samples_to_remove = hl.import_table(samples_to_remove_filename).key_by('s')
-    mt_filtered = mt.filter_cols(hl.is_defined(
-        samples_to_remove[mt.s]), keep=False)
-    mt_filtered.write(
-        f'{tmp_dir}/ddd-elgh-ukbb/Sanger_cohorts_chr1-7and20_split_sampleqc_filtered.mt')
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
+    # you may use your own here from your .s3fg file in your home directory
+    hadoop_config = sc._jsc.hadoopConfiguration()
+
+    hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
+    hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
+
+    #####################################################################
+    ###################### INPUT DATA  ##############################
+    #####################################################################
+    parser = argparse.ArgumentParser()
+    # Read the matrixtable, chrX and chrY should be included
+    input_params = parser.add_argument_group("Input parameters")
+    input_params.add_argument(
+        "--matrixtable",
+        help="Full path of input matrixtable. Path format \"file:///home/ubuntu/data/tmp/path/to/.mt\"",
+        default=f'{temp_dir}/ddd-elgh-ukbb/variant_qc/Sanger_cohorts_chr1-7and20_split.mt',
+        type=str,
+    )
+    input_params.add_argument(
+        "--failed_samples_list",
+        help="Full path of tsv file with list of failed samples, one per line",
+        default=f"{temp_dir}/ddd-elgh-ukbb/filtering/samples_failed_QC.tsv",
+        type=str,
+    )
+
+    input_params.add_argument(
+        "--output_dir",
+        help="Full path of output folder to store results. Preferably hdfs or secure lustre",
+        default=tmp_dir,
+        type=str
+    )
+
+    args = parser.parse_args()
+    main(args)
