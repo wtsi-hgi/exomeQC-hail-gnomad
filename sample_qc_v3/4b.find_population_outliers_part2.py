e0550cbfcd1a9ee9a905192b03d697c69dc9e43b
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-03-15 11:06:51 +0000

    4b script done

--- sample_qc_v3/4b.find_population_outliers_part2.py
+++ sample_qc_v3/4b.find_population_outliers_part2.py
@@ -10,6 +10,7 @@ import sys
 import re
 from pathlib import Path
 import logging
+import argparse
 from typing import Any, Counter, List, Optional, Tuple, Union, Dict, Iterable
 
 from bokeh.plotting import output_file, save, show
@@ -19,41 +20,9 @@ logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-
-def get_reference_genome(
-    locus: Union[hl.expr.LocusExpression, hl.expr.IntervalExpression],
-    add_sequence: bool = False,
-) -> hl.ReferenceGenome:
-    """
-    Returns the reference genome associated with the input Locus expression
-    :param locus: Input locus
-    :param add_sequence: If set, the fasta sequence is added to the reference genome
-    :return: Reference genome
-    """
-    if isinstance(locus, hl.expr.LocusExpression):
-        ref = locus.dtype.reference_genome
-    else:
-        assert isinstance(locus, hl.expr.IntervalExpression)
-        ref = locus.dtype.point_type.reference_genome
-    if add_sequence:
-        ref = add_reference_sequence(ref)
-    return ref
-
-
-def filter_to_autosomes(
-    t: Union[hl.MatrixTable, hl.Table]
-) -> Union[hl.MatrixTable, hl.Table]:
-    """
-    Filters the Table or MatrixTable to autosomes only.
-    This assumes that the input contains a field named `locus` of type Locus
-    :param t: Input MT/HT
-    :return:  MT/HT autosomes
-    """
-    reference = get_reference_genome(t.locus)
-    autosomes = hl.parse_locus_interval(
-        f"{reference.contigs[0]}-{reference.contigs[21]}", reference_genome=reference
-    )
-    return hl.filter_intervals(t, [autosomes])
+tmp_dir = "hdfs://spark-master:9820/"
+temp_dir = "file:///home/ubuntu/data/tmp"
+plot_dir = "/home/ubuntu/data/tmp"
 
 
 project_root = Path(__file__).parent.parent
@@ -77,36 +46,12 @@ with open(f"{storage}", 'r') as f:
 with open(f"{thresholds}", 'r') as f:
     thresholds = json.load(f)
 
-if __name__ == "__main__":
-    # need to create spark cluster first before intiialising hail
-    sc = pyspark.SparkContext()
-    # Define the hail persistent storage directory
-    tmp_dir = "hdfs://spark-master:9820/"
-    temp_dir = os.path.join(os.environ["HAIL_HOME"], "tmp")
-    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
-    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
-    # you may use your own here from your .s3fg file in your home directory
-    hadoop_config = sc._jsc.hadoopConfiguration()
 
-    hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
-    hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
-
-    bed_to_exclude_pca = hl.import_bed(
-        f"{temp_dir}/1000g/price_high_ld.bed.txt", reference_genome='GRCh38')
-    cohorts_pop = hl.import_table(
-        "s3a://DDD-ELGH-UKBB-exomes/ancestry/sanger_cohort_known_populations_ukbb_elgh_labels_updated.tsv", delimiter="\t").key_by('s')
-    # Read mt
-    mt = hl.read_matrix_table(
-        f"{temp_dir}/ddd-elgh-ukbb/new_labels/chr1_chr20_ldpruned_updated.mt")
-    # pca_scores_pop
-    pca_scores_pop = hl.read_table(
-        f"{temp_dir}/ddd-elgh-ukbb/new_labels/pop_assignments_updated.ht")
+def main(args):
 
     strata = {}
-    mt_with_sampleqc = hl.read_matrix_table(
-        f"{tmp_dir}/ddd-elgh-ukbb/mt_pops_superpops_sampleqc.mt")
-    pop_ht = hl.read_table(
-        f"{tmp_dir}/ddd-elgh-ukbb/mt_pops_superpops_sampleqc.ht")
+
+    pop_ht = hl.read_table(args.pop_ht)
     # run function on metrics including heterozygosity first for pops:
     strata['assigned_pop'] = pop_ht.assigned_pop
 
@@ -129,13 +74,49 @@ if __name__ == "__main__":
     #   hl.len(pop_ht.qc_metrics_filters) == 0))
     #logger.info(f'{checkpoint} exome samples found passing pop filtering')
     pop_filter_ht.write(
-        f"{tmp_dir}/ddd-elgh-ukbb/mt_pops_QC_filters.ht", overwrite=True)
+        f"{args.output_dir}/ddd-elgh-ukbb/mt_pops_QC_filters.ht", overwrite=True)
 
     # run function on metrics including heterozygosity  for superpops:
-    strata = {}
-    strata['assigned_superpop'] = pop_ht.assigned_superpop
-    pop_filter_ht_superpop = compute_stratified_metrics_filter(
-        pop_ht, qc_metrics, strata)
+    #strata = {}
+    #strata['assigned_superpop'] = pop_ht.assigned_superpop
+    # pop_filter_ht_superpop = compute_stratified_metrics_filter(
+    #    pop_ht, qc_metrics, strata)
 
-    pop_filter_ht_superpop.write(
-        f"{tmp_dir}/ddd-elgh-ukbb/mt_superpops_QC_filters.ht", overwrite=True)
+    # pop_filter_ht_superpop.write(
+    #    f"{tmp_dir}/ddd-elgh-ukbb/mt_superpops_QC_filters.ht", overwrite=True)
+
+
+if __name__ == "__main__":
+    # need to create spark cluster first before intiialising hail
+    sc = pyspark.SparkContext()
+    # Define the hail persistent storage directory
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
+    # you may use your own here from your .s3fg file in your home directory
+    hadoop_config = sc._jsc.hadoopConfiguration()
+
+    hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
+    hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
+
+    #####################################################################
+    ###################### INPUT DATA  ##############################
+    #####################################################################
+    parser = argparse.ArgumentParser()
+    # Read the matrixtable, chrX and chrY should be included
+    input_params = parser.add_argument_group("Input parameters")
+    input_params.add_argument(
+        "--pop_ht",
+        help="Full path of population PCA scores hail table from step 4a. ",
+        default=f"{tmp_dir}/ddd-elgh-ukbb/mt_pops_superpops_sampleqc.ht",
+        type=str,
+    )
+
+    input_params.add_argument(
+        "--output_dir",
+        help="Full path of output folder to store results. Preferably hdfs or secure lustre",
+        default=tmp_dir,
+        type=str
+    )
+
+    args = parser.parse_args()
+    main(args)
