7270573e6f540cc775fbca2958403d2d3b4b1c87
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-05-19 13:06:05 +0100

    train RF lustre

--- variant_qc/3.train_apply_finalise_RF_lustre.py
+++ variant_qc/3.train_apply_finalise_RF_lustre.py
@@ -560,7 +560,7 @@ if __name__ == "__main__":
         help='The specified interval(s) will be held out for testing and evaluation only. (default to "chr20")',
         nargs="+",
         type=str,
-        default="chr20:1-54444160",
+        default="chr3",
     )
     rf_params.add_argument(
         "--num_trees",
