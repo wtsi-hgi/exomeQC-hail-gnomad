424062cba3a3ac3c4c289ae1ca196b7581c3c46d
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-06-10 23:26:23 +0100

    rank bin with new file

--- variant_qc/4.rank_bin_plot_v2_denovo_singletons_lustre.py
+++ variant_qc/4.rank_bin_plot_v2_denovo_singletons_lustre.py
@@ -414,8 +414,8 @@ def main(args):
                              #score_expr=ht.rf_probability["TP"],
                              subrank_expr={
                                  'singleton_rank': ht.transmitted_singleton,
-                                 'biallelic_rank': ~ht.was_split,
-                                 'biallelic_singleton_rank': ~ht.was_split & ht.transmitted_singleton,
+                                 #'biallelic_rank': ~ht.was_split,
+                                 #'biallelic_singleton_rank': ~ht.was_split & ht.transmitted_singleton,
                                  'de_novo_high_quality_rank': ht.de_novo_data.p_de_novo[0] > 0.9,
                                  'de_novo_medium_quality_rank': ht.de_novo_data.p_de_novo[0] > 0.5,
                                  'de_novo_synonymous_rank': ht.consequence == "synonymous_variant",
