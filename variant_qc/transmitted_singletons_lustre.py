691639eac50471f9a2c21d3f0f1050af74d1f034
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-06-17 09:39:31 +0100

    trans singletons lustre

--- variant_qc/transmitted_singletons_lustre.py
+++ variant_qc/transmitted_singletons_lustre.py
@@ -133,9 +133,10 @@ if __name__ == "__main__":
 
     mt_untrans_count = mt_untrans.group_cols_by(mt_untrans.id).aggregate(
     untransmitted_singletons_count=hl.agg.count_where(
-                     (mt_untrans.proband_entry.GT.is_hom_ref()) & (
-                     (mt_untrans.father_entry.GT.is_non_ref()) |
-                     (mt_untrans.mother_entry.GT.is_non_ref()) )
+                     (mt_untrans.proband_entry.GT.is_hom_ref()) 
+                     # & (
+                     #(mt_untrans.father_entry.GT.is_non_ref()) |
+                     #(mt_untrans.mother_entry.GT.is_non_ref()) )
                                                      ))
     
 Total_transmitted_singletons=mt_trans_count.aggregate_entries(hl.agg.count_where(mt_trans_count.transmitted_singletons_count >0))
