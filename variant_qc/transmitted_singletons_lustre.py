2894f21d82d63e059d0e39060d49c2caaec86400
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-06-17 13:07:08 +0100

    trans singletons lustre

--- variant_qc/transmitted_singletons_lustre.py
+++ variant_qc/transmitted_singletons_lustre.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     #.filter_rows(mt_filtered.info.AC[0] ==, keep=True)
     print(mt_filtered.info.AC.summarize())
     print(mt_filtered.info.AC.show())
-    mt_trans_count=mt_trans.group_cols_by(mt_trans.id).aggregate(transmitted_singletons_count=hl.agg.count_where((mt_trans.info.AC[0] == 2) 
+    mt_trans_count=mt_trans.group_cols_by(mt_trans.locus,mt_trans.id).aggregate(transmitted_singletons_count=hl.agg.count_where((mt_trans.info.AC[0] == 2) 
                                 & (mt_trans.proband_entry.GT ==hl.Call([0,1])) & 
                                                                                             
                                ( ( (mt_trans.father_entry.GT ==hl.Call([0,1])) & (mt_trans.mother_entry.GT ==hl.Call([0,0])) )
