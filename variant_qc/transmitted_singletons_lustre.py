1c4f0886a7431620a57014be90b1c8f2cb7ba770
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-06-17 12:39:02 +0100

    trans singletons lustre

--- variant_qc/transmitted_singletons_lustre.py
+++ variant_qc/transmitted_singletons_lustre.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     untransmitted_singletons_count=hl.agg.count_where(
                      (mt_untrans.proband_entry.GT.is_non_ref()) 
                       & 
-                      (  (mt_trans.father_entry.GT.is_non_ref()) | (mt_trans.mother_entry.GT.is_non_ref() ))
+                      (  (mt_untrans.father_entry.GT.is_non_ref()) | (mt_untrans.mother_entry.GT.is_non_ref() ))
                                                                                                              ))
                       # 
                      # (
