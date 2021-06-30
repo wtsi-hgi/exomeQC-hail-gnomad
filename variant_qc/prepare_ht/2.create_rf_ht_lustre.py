82a42723120dec51e42d89ef0016e4df7478ec6f
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-06-30 11:58:41 +0100

    removed filtering step to only variants found in high quality samples

--- variant_qc/prepare_ht/2.create_rf_ht_lustre.py
+++ variant_qc/prepare_ht/2.create_rf_ht_lustre.py
@@ -162,9 +162,9 @@ def main(args):
         **allele_counts_ht[ht.key],
     )
     # Filter to only variants found in high quality samples or controls with no LowQual filter
-    ht = ht.filter(
-        (ht[f"ac_children_{group}"] > 0)
-     )  # TODO: change to AS_lowqual for v3.1 or leave as is to be more consistent with v3.0? I will need to add this annotation if so
+    #ht = ht.filter(
+    #    (ht[f"ac_children_{group}"] > 0)
+    # )  # TODO: change to AS_lowqual for v3.1 or leave as is to be more consistent with v3.0? I will need to add this annotation if so
     ht = ht.annotate(fail_hard_filters=(ht.QD < 2)
                      | (ht.FS > 60) | (ht.MQ < 30))
     ht = ht.annotate(ac_raw=ht.ac_qc_samples_raw)
