f505471b5d8c1b985992a22dc2feff610ee69ac7
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-06-30 11:25:24 +0100

    annotated with both allele_data substructure and main structure to get all features names

--- variant_qc/prepare_ht/2.create_rf_ht_lustre.py
+++ variant_qc/prepare_ht/2.create_rf_ht_lustre.py
@@ -147,7 +147,7 @@ def main(args):
        ~hl.is_nan(mt.info.InbreedingCoeff), mt.info.InbreedingCoeff))
     ht = mt.rows()
     ht = ht.transmute(**ht.info)
-    ht = ht.select( "MQ", "QD", "InbreedingCoeff", *INFO_FEATURES)
+    ht = ht.select( "MQ", "InbreedingCoeff", *INFO_FEATURES)
 
     trio_stats_ht = trio_stats_table.select(
         f"n_transmitted_{group}", f"ac_children_{group}"
