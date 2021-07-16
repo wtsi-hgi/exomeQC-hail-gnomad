fb2191cc3ea95d2d2ed8b2612fd465ebd4364eec
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-07-16 14:12:57 +0100

    yet another transmitted singletons calculation

---
+++ variant_qc/random_forest/transmitted_singletons_trios_KS.py
@@ -0,0 +1,93 @@
+# Pavlos Antoniou
+# 16/09/2020
+#  trio matrixtable creation from fam file
+import os
+import hail as hl
+import pandas as pd
+import pyspark
+import json
+import sys
+import re
+from pathlib import Path
+import logging
+from typing import Any, Counter, List, Optional, Tuple, Union, Dict
+from bokeh.plotting import output_file, save, show
+from gnomad.resources.grch38 import gnomad
+from gnomad.utils.annotations import annotate_adj
+from gnomad.utils.annotations import unphase_call_expr, add_variant_type
+from gnomad.utils.annotations import annotate_adj, bi_allelic_expr, bi_allelic_site_inbreeding_expr
+from gnomad.utils.filtering import filter_to_autosomes
+from gnomad.utils.filtering import filter_to_adj
+from gnomad.sample_qc.relatedness import (
+    SIBLINGS,
+    generate_sib_stats_expr,
+    generate_trio_stats_expr,
+)
+
+from hail import Table
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+project_root = Path(__file__).parent.parent.parent
+print(project_root)
+
+s3credentials = os.path.join(
+    project_root, "hail_configuration_files/s3_credentials.json")
+print(s3credentials)
+
+storage = os.path.join(project_root, "hail_configuration_files/storage.json")
+
+thresholds = os.path.join(
+    project_root, "hail_configuration_files/thresholds.json")
+
+with open(f"{s3credentials}", 'r') as f:
+    credentials = json.load(f)
+
+with open(f"{storage}", 'r') as f:
+    storage = json.load(f)
+
+with open(f"{thresholds}", 'r') as f:
+    thresholds = json.load(f)
+
+
+tmp_dir = "hdfs://spark-master:9820/"
+temp_dir = "file:///home/ubuntu/data/tmp"
+plot_dir = "/home/ubuntu/data/tmp"
+lustre_dir = "file:///lustre/scratch123/teams/hgi/mercury/megaWES-variantqc"
+
+
+def main():
+    run_hash="91ba5f38"
+    ht = hl.read_table(
+        f'{lustre_dir}/variant_qc/models/{run_hash}/rf_result_MegaWES_new.ht')
+    accessions=hl.import_table(f'{lustre_dir}/kaitlin_trios/forPavlos_100trios_EGA_accessions.txt',no_header=False).key_by('s')   
+    mt_trios = hl.read_matrix_table(
+        f'{lustre_dir}/variant_qc/MegaWES_trios_adj.mt')
+    samples=accessions.s.collect()
+
+    mt_100_trios=mt_trios.filter_cols(samples.contains(mt_trios['s']))
+    print(mt_trios.count())
+
+    print(mt_100_trios.count())
+if __name__ == "__main__":
+    # need to create spark cluster first before intiialising hail
+    sc = pyspark.SparkContext()
+    # Define the hail persistent storage directory
+  
+    hl.init(sc=sc, tmp_dir=lustre_dir, local_tmpdir=lustre_dir, default_reference="GRCh38")
+
+    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
+    # you may use your own here from your .s3fg file in your home directory
+    hadoop_config = sc._jsc.hadoopConfiguration()
+
+    hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
+    hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
+
+    ################################
+
+    #################################
+
+    main()
+   
