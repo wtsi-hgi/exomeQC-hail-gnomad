b3889e7d43c74dafa65d36d8e0153c42a67795b1
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-07-19 15:37:23 +0100

    het_ref to non_ref

--- variant_qc/random_forest/transmitted_singletons_trios_KS.py
+++ variant_qc/random_forest/transmitted_singletons_trios_KS.py
@@ -62,14 +62,20 @@ def count_trans_untransmitted_singletons(mt_filtered, ht):
     
     mt_trans = mt_filtered.filter_entries(mt_filtered.info.AC[0] == 2)
     mt_untrans = mt_filtered.filter_entries(mt_filtered.info.AC[0] == 1)
-    
-    mt_trans_count=mt_trans.group_cols_by(mt_trans.id).aggregate(transmitted_singletons_count=hl.agg.count_where(
+    '''
+      mt_trans_count=mt_trans.group_cols_by(mt_trans.id).aggregate(transmitted_singletons_count=hl.agg.count_where(
                                 (mt_trans.info.AC[0] == 2) &
-                                (mt_trans.proband_entry.GT.is_non_ref()) &
+                                (mt_trans.proband_entry.GT.is_non_ref() ) &
+                                ((mt_trans.father_entry.GT.is_non_ref()) |
+                                (mt_trans.mother_entry.GT.is_non_ref()))))
+    '''
+    mt_trans_count=mt_trans.group_cols_by(mt_trans.id).aggregate(transmitted_singletons_count=hl.agg.count_where(
+                               
+                                (mt_trans.proband_entry.GT.is_non_ref() ) &
                                 ((mt_trans.father_entry.GT.is_non_ref()) |
                                 (mt_trans.mother_entry.GT.is_non_ref()))))
     
-    Total_transmitted_singletons=mt_trans_count.aggregate_entries(hl.agg.count_where(mt_trans_count.transmitted_singletons_count >= 1))
+    Total_transmitted_singletons=mt_trans_count.aggregate_entries(hl.agg.count_where(mt_trans_count.transmitted_singletons_count == 1))
     print(Total_transmitted_singletons)
     mt_untrans_count = (mt_untrans.group_cols_by(mt_untrans.id).aggregate(
     untransmitted_singletons_count=hl.agg.count_where(
