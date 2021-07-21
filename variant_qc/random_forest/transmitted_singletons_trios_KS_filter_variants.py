9511bd6fe01458618f02cc0aa7fbcac74abd6be8
Pavlos Antoniou <pa10@sanger.ac.uk>
2021-07-21 14:18:53 +0100

    no filter of samples just variants

--- variant_qc/random_forest/transmitted_singletons_trios_KS_filter_variants.py
+++ variant_qc/random_forest/transmitted_singletons_trios_KS_filter_variants.py
@@ -79,7 +79,7 @@ def count_trans_untransmitted_singletons(mt_filtered, ht):
                                 ))
     
     '''
-    mt_trans_count=mt_trans.aggregate_rows(transmitted_singletons_count=hl.agg.count_where(
+    mt_trans_count=mt_trans.group_cols_by(mt_trans.proband_entry.id,mt_trans.mother_entry.id, mt_trans.father_entry.id)(transmitted_singletons_count=hl.agg.count_where(
                                # (mt_trans.info.AC[0] == 2) &
                                 (mt_trans.proband_entry.GT.is_non_ref()) &
                                 (
